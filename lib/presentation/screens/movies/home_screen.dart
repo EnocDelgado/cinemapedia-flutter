343e49969436c9c35a3fe7cc18dd17e525d48c75
Enoc Delgado <105158271+EnocDelgado@users.noreply.github.com>
2023-06-15 10:42:28 -0500

    movies slider created

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,5 +1,5 @@
 
-import 'package:cinemapedia/presentation/providers/movies/movies_providers.dart';
+import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -37,24 +37,15 @@ class _HomeViewState extends ConsumerState<_HomeView> {
   @override
   Widget build(BuildContext context) {
 
-    final nowPlayingMovies = ref.watch( nowPlayingMoviesProvider );
+    // final nowPlayingMovies = ref.watch( nowPlayingMoviesProvider );
+    final nowPlayingMovies = ref.watch( moviesSlideshowProvider );
 
     return Column(
       children: [
 
-        CustomAppBar(),
+        const CustomAppBar(),
 
-        Expanded(
-          child:  ListView.builder(
-            itemCount: nowPlayingMovies.length,
-            itemBuilder: (context, index) {
-              final movie = nowPlayingMovies[ index ];
-              return ListTile(
-                title: Text( movie.title ),
-              );
-            },
-          ),
-        )
+        MoviesSlideshow( movies: nowPlayingMovies )
       ],
     );
   }
