f0f9108bea64ef715a9ee4559b14b10f5502c794
Enoc Delgado <105158271+EnocDelgado@users.noreply.github.com>
2023-06-14 23:18:31 -0500

    providers created and request successful

---
+++ lib/presentation/providers/movies/movies_providers.dart
@@ -0,0 +1,34 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/presentation/providers/movies/movies_repositorry_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final nowPlayingMoviesProvider = StateNotifierProvider<MoviesNotifier, List<Movie>>( (ref) {
+
+  // reference to provider
+  final fectMoreMovies = ref.watch( movieRepositoryProvider ).getNowPlaying;
+
+  return MoviesNotifier(
+    fetchMoreMovies: fectMoreMovies
+  );
+});
+
+typedef MovieCallback = Future<List<Movie>> Function({ int page });
+
+class MoviesNotifier extends StateNotifier<List<Movie>> {
+
+  int currentPage = 0;
+  MovieCallback fetchMoreMovies;
+
+  MoviesNotifier({
+    required this.fetchMoreMovies
+  }): super([]);
+
+  Future<void> loadNextPage() async {
+    currentPage++;
+
+    final List<Movie> movies = await fetchMoreMovies( page: currentPage );
+
+    state = [ ...state, ...movies ];
+  }
+
+}
